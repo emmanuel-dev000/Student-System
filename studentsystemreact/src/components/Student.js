104e17f72970fd3618d517d4bb306940793cc8ef
emmanuel-dev000 <emmanuelpanganwork@gmail.com>
2023-05-27 13:19:59 +0800

    Added frontend page

--- studentsystemreact/src/components/Student.js
+++ studentsystemreact/src/components/Student.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { Container, Paper } from "@mui/material";
+import { Container, Paper, Button } from "@mui/material";
 
 export default function StudentTextFields() {
     const paperStyle = {
@@ -10,22 +10,32 @@ export default function StudentTextFields() {
         margin: "20px auto",
     };
 
+    const [name, setName] = React.useState("Name");
+    const [address, setAddress] = React.useState("Address");
+    const onSubmitButtonClick = (e) => {
+        e.preventDefault();
+        const student = { name, address };
+        console.log(student);
+    };
+
+
   return (
     <Container>
         
         <Paper elevation={ 3 } style={ paperStyle }>
-            <Box
-            component="form"
-            sx={{
-                '& > :not(style)': { m: 1 },
-            }}
-            noValidate
-            autoComplete="off"
-            >
-                <h1>Add Student</h1>
-            <TextField id="outlined-basic" label="Student Name" variant="outlined" fullWidth/>
-            <TextField id="outlined-basic" label="Student Address" variant="outlined" fullWidth/>
+            <Box component="form" sx={{ '& > :not(style)': { m: 1 }, }} noValidate autoComplete="off">
+            <h1>Add Student</h1>
+            <TextField id="outlined-basic" label="Student Name" variant="outlined" fullWidth
+                value={ name } onChange={ (e) => setName(e.target.value) }
+                />
+            <TextField id="outlined-basic" label="Student Address" variant="outlined" fullWidth
+                value={ address } onChange={ (e) => setAddress(e.target.value) }
+            />
+            { name } lives at { address }
             </Box>
+            <Button variant="contained" size="large" onClick={ onSubmitButtonClick }>
+                Submit
+            </Button>
         </Paper>
     </Container>
   );
