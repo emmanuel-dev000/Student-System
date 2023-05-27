a0f93ff00ee8ba1a45be0c0c06a020005b538caf
emmanuel-dev000 <emmanuelpanganwork@gmail.com>
2023-05-27 11:52:10 +0800

    Initialize studentsystemreact for frontend

---
+++ studentsystemreact/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Appbar from "./components/Appbar";
+import StudentTextFields from "./components/Student";
+
+function App() {
+  return (
+    <div className="App">
+      <Appbar/>
+      <StudentTextFields/>
+    </div>
+  );
+}
+
+export default App;
---
+++ studentsystemreact/src/components/Student.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Container } from "@mui/material";
+
+export default function StudentTextFields() {
+  return (
+    <Container>
+        <Box
+        component="form"
+        sx={{
+            '& > :not(style)': { m: 1 },
+        }}
+        noValidate
+        autoComplete="off"
+        >
+        <TextField id="outlined-basic" label="Student Name" variant="outlined" />
+        <TextField id="outlined-basic" label="Student Address" variant="outlined" />
+        </Box>
+    </Container>
+  );
+}
