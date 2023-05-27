a9e39b5542a6593b042c38d1f8c284a4a2fe0f68
emmanuel-dev000 <emmanuelpanganwork@gmail.com>
2023-05-27 12:02:12 +0800

    Added Paper component to Student.js

--- studentsystemreact/src/components/Student.js
+++ studentsystemreact/src/components/Student.js
@@ -1,22 +1,30 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { Container } from "@mui/material";
+import { Container, Paper } from "@mui/material";
 
 export default function StudentTextFields() {
+    const paperStyle = {
+        padding: "50px 20px",
+        width: 200,
+        margin: "20px auto",
+    };
   return (
     <Container>
-        <Box
-        component="form"
-        sx={{
-            '& > :not(style)': { m: 1 },
-        }}
-        noValidate
-        autoComplete="off"
-        >
-        <TextField id="outlined-basic" label="Student Name" variant="outlined" />
-        <TextField id="outlined-basic" label="Student Address" variant="outlined" />
-        </Box>
+        
+        <Paper elevation={ 3 } style={ paperStyle }>
+            <Box
+            component="form"
+            sx={{
+                '& > :not(style)': { m: 1 },
+            }}
+            noValidate
+            autoComplete="off"
+            >
+            <TextField id="outlined-basic" label="Student Name" variant="outlined" />
+            <TextField id="outlined-basic" label="Student Address" variant="outlined" />
+            </Box>
+        </Paper>
     </Container>
   );
 }
